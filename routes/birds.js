e53dbf8d87609e2653a4f4dc0a63295bd2437cca
Samantha Ågren <sammi.agren@elev.ga.ntig.se>
2024-12-10 10:48:32 +0100

    set up lots of things: - basic page containing all birds - form for adding new bird
    things to continue working on:
    - singular bird page with more information
    - add species page
    - redirect from added bird to that birds individual page
    - link up bird creation page with more things (species)
    - UI/UX

--- routes/birds.js
+++ routes/birds.js
@@ -6,7 +6,40 @@ const router = express.Router()
 router.get("/", async (request, response) => {
     const [birds] = await pool.promise().query('SELECT * FROM birds')
 
-    response.json(birds)
+    response.render("birds.njk", {
+        birds
+    })
+})
+
+router.get("/create", async (request, response) => {
+
+    const [validOwners] = await pool.promise().query('SELECT id, name FROM owners')
+
+    response.render("creationform.njk", {
+        validOwners
+    })
+})
+
+router.post("/", async (request, response) => {
+    console.log(request.body)
+    const {name, species, owner, wingspan, weight} = request.body
+    const birdName = name
+    const ownerName = owner
+
+    const [[speciesID]] = await pool.promise().query(
+        `SELECT id 
+        FROM species 
+        WHERE name = ?`,
+        species)
+
+    console.log(speciesID)
+
+    const [result] = await pool.promise().query(
+        `INSERT INTO birds (name, wingspan, weight, species_id) 
+        VALUES (?, ?, ?, ?)`, 
+        [birdName, wingspan, weight, speciesID])
+
+    response.redirect("/birds/", result.insertId)
 })
 
 router.get("/login", (request, response) => {
