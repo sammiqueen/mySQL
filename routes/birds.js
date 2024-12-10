16204b135d58b6dd21948cef2dc58585a7bd95e9
Samantha Ågren <sammi.agren@elev.ga.ntig.se>
2024-12-10 11:13:45 +0100

    stuff

--- routes/birds.js
+++ routes/birds.js
@@ -13,10 +13,14 @@ router.get("/", async (request, response) => {
 
 router.get("/create", async (request, response) => {
 
-    const [validOwners] = await pool.promise().query('SELECT id, name FROM owners')
+    const [options] = await pool.promise().query(
+        `SELECT id AS ownerID, name AS ownerName
+        FROM owners 
+        JOIN id AS speciesID, name AS speciesName
+        FROM species`)
 
     response.render("creationform.njk", {
-        validOwners
+        options
     })
 })
 
@@ -24,7 +28,6 @@ router.post("/", async (request, response) => {
     console.log(request.body)
     const {name, species, owner, wingspan, weight} = request.body
     const birdName = name
-    const ownerName = owner
 
     const [[speciesID]] = await pool.promise().query(
         `SELECT id 
@@ -34,10 +37,23 @@ router.post("/", async (request, response) => {
 
     console.log(speciesID)
 
+    const [[ownerID]] = await pool.promise().query(
+        `SELECT id 
+        FROM owners 
+        WHERE name = ?`,
+        owner)
+
+    console.log(ownerID)
+
     const [result] = await pool.promise().query(
         `INSERT INTO birds (name, wingspan, weight, species_id) 
         VALUES (?, ?, ?, ?)`, 
         [birdName, wingspan, weight, speciesID])
+
+    await pool.promise().query(
+        `INSERT INTO owners_birds (owner_id, bird_id)
+        VALUES (?, ?)`,
+        ownerID, result.insertId)
 
     response.redirect("/birds/", result.insertId)
 })
